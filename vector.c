93314e8baea973ba047252b47e8081db8b926bc1
cleverkirik <k7lisenk@gmail.com>
2025-12-09 22:55:02 +0300

    any commit description

--- vector.c
+++ vector.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef struct vector_element {
-    void *value;
-
-} vector_element;
+#define VECTOR_INIT_SIZE 100
 
 typedef struct vector {
     size_t element_size;
     uint32_t vector_size;
+    void *data;
+
 } vector;
 
-vector *init_vector() {
-
+vector *init_vector(size_t element_size) {
+    vector *vec = malloc(sizeof(vector));
+    vec->element_size = element_size;
+    vec->data = malloc(VECTOR_INIT_SIZE * element_size);
 }
 
 void free_vector(vector *vec) {
+    free(vec->data);
+    free(vec);
+}
+
+void vector_extend(vector *vec) {
 
 }
 
 uint32_t get_vector_size(vector *vec) {
-
+    return vec->vector_size; 
 }
 
-vector_element *get_vector_element(vector *vec, uint32_t i) {
+void *vector_begin(vector *vec) {
+    return vec->data;
+}
 
+void *vector_end(vector *vec) {
+    return (void*)((char*)vector_begin(vec) + vec->vector_size * vec->element_size);
+}
+
+void *get_vector_element(vector *vec, uint32_t i) {
+    if (i < vec->vector_size) return (void*)((char*)vector_begin(vec) + i * vec->element_size);
+    else return NULL;
 }
 
 void push_element(vector *vec, uint32_t i, void *value) {
